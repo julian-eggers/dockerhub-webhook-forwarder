896e27e3db876a680056c5fee96b2c41bf91a603
Julian Eggers <eggers.julian@gmail.com>
2017-01-15 13:27:55 +0100

    0.4.1-RELEASE

--- src/main/java/com/itelg/docker/dwf/domain/WebhookEvent.java
+++ src/main/java/com/itelg/docker/dwf/domain/WebhookEvent.java
@@ -2,10 +2,12 @@ package com.itelg.docker.dwf.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
-@Data
+@Getter
+@Setter
 @ToString(exclude = "originalJson")
 public class WebhookEvent
 {
